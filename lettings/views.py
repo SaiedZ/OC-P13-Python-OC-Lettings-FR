3876a8dc94e338fed625a3b63af3e3d81a9dc94a
Deias <saied_am@hotmail.com>
2022-06-06 23:43:04 +0200

    refactor: moving the templates related to lettings app into the tmplates folder in the app. lettings_index.html were named index.html and the lettings_index view named to index too.

--- lettings/views.py
+++ lettings/views.py
@@ -14,10 +14,10 @@ from .models import Letting
 """
 
 
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 """
@@ -41,4 +41,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
