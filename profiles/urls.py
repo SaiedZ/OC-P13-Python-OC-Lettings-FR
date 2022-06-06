c37714bf70e3bc2b388d9790a4ae13916e606e57
Deias <saied_am@hotmail.com>
2022-06-06 23:22:35 +0200

    refactor: moving the profiles views and urls to the profiles app

---
+++ profiles/urls.py
@@ -0,0 +1,12 @@
+"""
+Urls for profiles app.
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('profiles/', views.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', views.profile, name='profile'),
+]
