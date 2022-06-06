fed442abf92d8b038e4773da2da49933cce91d30
Deias <saied_am@hotmail.com>
2022-06-06 23:45:46 +0200

    refactor: moving profiles template to the app's templates folder. changing the name of the profiles_index view to index and the name of the html files profiles_index.html to index.html

--- profiles/views.py
+++ profiles/views.py
@@ -13,10 +13,10 @@ from .models import Profile
 """
 
 
-def profiles_index(request):
+def index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 
 """
@@ -30,4 +30,4 @@ def profiles_index(request):
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
