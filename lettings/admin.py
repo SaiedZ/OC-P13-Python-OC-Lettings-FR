528decf7f39fe83962800aebdc7d13624315e37d
Deias <saied_am@hotmail.com>
2022-06-06 22:31:10 +0200

    refactor: moving the registration of the lettings  models in the admin site to the letting app

--- lettings/admin.py
+++ lettings/admin.py
@@ -2,11 +2,11 @@
 Admin config for letting app.
 """
 
-'''from django.contrib import admin
+from django.contrib import admin
 
 from .models import Letting
 from .models import Address
 
 
 admin.site.register(Letting)
-admin.site.register(Address)'''
+admin.site.register(Address)
