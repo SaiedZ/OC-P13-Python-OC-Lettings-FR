d143cb6ea44b5af2a0052f2d46826c49d634c2b8
Deias <saied_am@hotmail.com>
2022-06-07 21:16:08 +0200

    tests: adding test for profile detaile view, 'profile', to the profiles app

--- profiles/tests.py
+++ profiles/tests.py
@@ -6,6 +6,10 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 
+from django.contrib.auth import get_user_model
+from profiles import models
+
+USER_MODEL = get_user_model()
 PROFILE_INDEX_URL = reverse("profiles_index")
 
 
@@ -25,3 +29,18 @@ class ProfilesTests(TestCase):
         res = self.client.get(PROFILE_INDEX_URL)
         self.assertInHTML("Profiles", res.content.decode())
         self.assertContains(res, "Profiles")
+
+    def test_profile_page(self):
+        """Test http response and data in the profile page."""
+        profile = models.Profile.objects.create(
+            favorite_city="City",
+            user=USER_MODEL.objects.create(
+                username='testuser',
+            ),
+        )
+        PROFILE_URL = reverse("profile", kwargs={'username': profile.user.username})
+        res = self.client.get(PROFILE_URL)
+
+        self.assertEqual(res.status_code, 200)
+        print(res.content.decode('utf-8'))
+        self.assertInHTML(f'<p>Favorite city: {profile.favorite_city}</p>', res.content.decode())
