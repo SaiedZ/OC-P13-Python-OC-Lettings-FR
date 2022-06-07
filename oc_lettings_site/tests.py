0df64cdfdddab333207fff22dcb50e8b82795656
Deias <saied_am@hotmail.com>
2022-06-07 21:19:56 +0200

    tests: adding tests for the oc_lettings_site index view

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,26 @@
-def test_dummy():
-    assert 1
+"""
+Tests for the index view.
+"""
+
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+
+INDEX_URL = reverse("index")
+
+
+class IndexTests(TestCase):
+    """Test index view."""
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_http_response_code_index(self):
+        """Test http response for the index page."""
+        res = self.client.get(INDEX_URL)
+        self.assertEqual(res.status_code, 200)
+
+    def test_http_response_content_index(self):
+        """Test http content for the index page."""
+        res = self.client.get(INDEX_URL)
+        self.assertInHTML("<h1>Welcome to Holiday Homes</h1>", res.content.decode())
