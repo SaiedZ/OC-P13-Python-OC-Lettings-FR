4906e88f2f7fbc3cfa8436fad730c8c41a245415
Deias <saied_am@hotmail.com>
2022-06-18 20:15:38 +0200

    doc: adding docstring for the lettings views

--- lettings/views.py
+++ lettings/views.py
@@ -6,21 +6,31 @@ from django.shortcuts import render
 from .models import Letting
 
 
-"""
 # Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat
 # massa. Integer est nunc, pulvinar a
 # tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
 # posuere cubilia curae; Cras eget scelerisque
-"""
 
 
 def index(request):
+    """The index function of the Letting model view.
+
+    This view will show all the lettings in the database.
+
+    Parameters
+    ----------
+    request : http request object.
+        The request sent by the client.
+
+    Returns
+    -------
+    A render of index.html page.
+    """
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings/index.html', context)
 
 
-"""
 #Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id
 # eleifend. Praesent dignissim, odio eu consequat pretium, purus urna vulputate arcu, vitae
 # efficitur
@@ -32,10 +42,23 @@ def index(request):
 # Vivamus consequat aliquam libero, eget bibendum lorem. Sed non dolor risus. Mauris condimentum
 # auctor elementum. Donec quis nisi ligula. Integer vehicula tincidunt enim, ac lacinia augue
 # pulvinar sit amet.
-"""
 
 
 def letting(request, letting_id):
+    """The letting view of the Letting model.
+
+    This view will show the detail of a letting using
+    the id in the request parameters.
+
+    Parameters
+    ----------
+    request : http request object.
+        The request sent by the client.
+
+    Returns
+    -------
+    A render of letting.html page.
+    """
     letting = Letting.objects.get(id=letting_id)
     context = {
         'title': letting.title,
