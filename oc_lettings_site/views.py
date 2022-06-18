6f2b2fe26895fd4e4cd0e7033af08dee59a51a89
Deias <saied_am@hotmail.com>
2022-06-18 20:21:06 +0200

    doc: adding docstring to the home page view (oc_lettings --> views --> index function

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 
-"""
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo
 # consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi
 # convallis eros,
@@ -9,8 +8,18 @@ from django.shortcuts import render
 # lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
 # Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi, pellentesque iaculis
 # enim cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
-"""
 
 
 def index(request):
+    """Index funtion for the home page view.
+
+    Parameters
+    ----------
+    request : http request object.
+        The request sent by the client.
+
+    Returns
+    -------
+    A render of index.html page of the home page.
+    """
     return render(request, 'index.html')
